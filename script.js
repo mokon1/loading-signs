76ac4a61b03f9236c99bba26896fb7432e18ef2b
mokon1 <michalina.okon@gmail.com>
2017-08-27 17:35:41 +0200

    added ribbon

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
     
+    var box_title = $('div.box_title');
+    
     $('h1').hide().delay(200).slideDown('slow');
     $('p').hide().delay(300).slideDown('slow');
     
-    $('form').hide().delay(500).fadeIn('slow');
+    $('form').hide().fadeIn('slow');
     
     $('.option1').hide().delay(1500).slideDown('slow');
     $('.option2').hide().delay(1600).slideDown('slow');
@@ -15,37 +17,37 @@ $(document).ready(function(){
         
     $('.option1').on('click',function(){
         $('.jumping_dots_container').css('display', 'flex');
-        $('.jumping_dots_container').siblings('div').css('display', 'none');
+        $('.jumping_dots_container').siblings('div').not(box_title).css('display', 'none');
         $('body').css('backgroundColor', 'darkblue');
         moveForm();
     });
     $('.option2').on('click',function(){
         $('.pulsing_dots_container').css('display', 'flex');
-        $('.pulsing_dots_container').siblings('div').css('display', 'none');
+        $('.pulsing_dots_container').siblings('div').not(box_title).css('display', 'none');
         $('body').css('backgroundColor', 'Purple');
         moveForm();
     });
     $('.option3').on('click',function(){
         $('.loading_rectangles_container').css('display', 'flex');
-        $('.loading_rectangles_container').siblings('div').css('display', 'none');
+        $('.loading_rectangles_container').siblings('div').not(box_title).css('display', 'none');
         $('body').css('backgroundColor', 'darkgreen');
         moveForm();
     });
     $('.option4').on('click',function(){
         $('.circles_container').css('display', 'flex');
-        $('.circles_container').siblings('div').css('display', 'none');
+        $('.circles_container').siblings('div').not(box_title).css('display', 'none');
         $('body').css('backgroundColor', 'DimGray');
         moveForm();
     });
     $('.option5').on('click',function(){
         $('.loading_figures_container').css('display', 'flex');
-        $('.loading_figures_container').siblings('div').css('display', 'none');
+        $('.loading_figures_container').siblings('div').not(box_title).css('display', 'none');
         $('body').css('backgroundColor', '#b3b300');
         moveForm();
     });
     $('.option6').on('click',function(){
         $('.loading_text_container').css('display', 'flex');
-        $('.loading_text_container').siblings('div').css('display', 'none');
+        $('.loading_text_container').siblings('div').not(box_title).css('display', 'none');
         $('body').css('backgroundColor', '#b30000');
         moveForm();
     });
